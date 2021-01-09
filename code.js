494e7a8c9580f9bcbc7bec0955208bfb30e7ea06
Ashton Simmonds <ashton.simmonds@gmail.com>
2021-01-09 21:03:51 +0900

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
 // This code puts the date into thing.
-document.getElementById('thing').innerHTML = Date()
+document.getElementById("thing").innerHTML = Date()
 // https://www.w3schools.com/js/js_date_methods.asp
